2a49a95226824109b42bfc4fb328200223735186
Steve Jewkes <steve@mulus.net>
2018-04-30 20:07:39 +0100

    Work in progress trying to create a useful solid polygon drawer

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -99,9 +99,83 @@ void fillpoly(int num_points, int *points)
 	}
 }
 
+void drawtri_sorted(int x0, int y0, int x1, int y1, int x2, int y2)
+{
+	float dx0 = 0.0f;
+	float dx1 = 0.0f;
+	float dx2 = 0.0f;
+	
+	if ((y1-y0) > 0)
+		dx0 = static_cast<float>(x1 - x0) / static_cast<float>(y1 - y0);
+	if ((y2-y0) > 0)
+		dx1 = static_cast<float>(x2 - x0) / static_cast<float>(y2 - y0);
+	if ((y2-y1) > 0)
+		dx2 = static_cast<float>(x2 - x1) / static_cast<float>(y2 - y1);
+
+	float s_x = static_cast<float>(x0);
+	int s_y = y0;
+	float e_x = static_cast<float>(x0);
+	int e_y = y0;
+
+	if (dx0 > dx1)
+	{
+		for (; s_y < y1; s_y++, e_y++, s_x += dx1, e_x += dx0)
+			line(static_cast<int>(s_x), s_y, static_cast<int>(e_x), s_y);
+		e_x = static_cast<float>(x1);
+		e_y = y1;
+		for (; s_y <= y2; s_y++, e_y++, s_x += dx1, e_x += dx2)
+			line(static_cast<int>(s_x), s_y, static_cast<int>(e_x), s_y);
+	}
+	else
+	{
+		for (; s_y < y1; s_y++, e_y++, s_x += dx0, e_x += dx1)
+			line(static_cast<int>(s_x), s_y, static_cast<int>(e_x), s_y);
+		s_x = static_cast<float>(x1);
+		s_y = y1;
+		for (; s_y <= y2; s_y++, e_y++, s_x += dx2, e_x += dx1)
+			line(static_cast<int>(s_x), s_y, static_cast<int>(e_x), s_y);
+	}
+}
+
 void drawtri(int x0, int y0, int x1, int y1, int x2, int y2)
 {
 	line(x0, y0, x1, y1);
 	line(x1, y1, x2, y2);
 	line(x2, y2, x0, y0);
+
+	/*
+	if (y0 > y1 && y0 > y2)
+	{
+		if (y1 > y2)
+		{
+			drawtri_sorted(x2, y2, x1, y1, x0, y0);
+		}
+		else
+		{
+			drawtri_sorted(x1, y1, x2, y2, x0, y0);
+		}
+	}
+	else if (y1 > y0 && y1 > y2)
+	{
+		if (y0 > y2)
+		{
+			drawtri_sorted(x2, y2, x0, y0, x1, y1);
+		}
+		else
+		{
+			drawtri_sorted(x0, y0, x2, y2, x1, y1);
+		}
+	}
+	else if (y2 > y0 && y2 > y1)
+	{
+		if (y0 > y1)
+		{
+			drawtri_sorted(x1, y1, x0, y0, x2, y2);
+		}
+		else
+		{
+			drawtri_sorted(x0, y0, x1, y1, x2, y2);
+		}
+	}
+	*/
 }
