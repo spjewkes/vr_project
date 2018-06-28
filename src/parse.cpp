f35cbbc80245afd3c8a63c6a6e75ca35e7bfe89d
Steve Jewkes <steve@mulus.net>
2018-06-28 16:18:20 +0100

    Remove unecessary includes

--- src/parse.cpp
+++ src/parse.cpp
@@ -10,9 +10,6 @@
 /****************
 * include files *
 ****************/
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "defs.hpp"
 #include "error.hpp"
 #include "parse.hpp"
