70dce56ea2aea2f9092456a3e89a48acb081f5d1
Steve Jewkes <steve@mulus.net>
2018-09-02 19:36:09 +0100

    Some tidying up of the shading calculation

--- src/instance.cpp
+++ src/instance.cpp
@@ -28,9 +28,7 @@ void Instance::setup_color()
 {
 	int no_polygons, loop;
 	int poly_no[3];
-	float x1,y1,z1, x2,y2,z2, x3,y3,z3;
-	float dx1,dy1,dz1, dx2,dy2,dz2;
-	float A,B,C,D, normal, hyp, cos_theta, ka, kd;
+	float ka, kd;
 
 	no_polygons = masterptr->poly0.size();
 
@@ -47,73 +45,26 @@ void Instance::setup_color()
 		poly_no[1] = masterptr->poly1[loop];
 		poly_no[2] = masterptr->poly2[loop];
 
-		/* get the first vertex of that edge */
-		x1 = vert[poly_no[0]].x();
-		y1 = vert[poly_no[0]].y();
-		z1 = vert[poly_no[0]].z();
-		/* now get the second vertex of that edge */
-		x2 = vert[poly_no[1]].x();
-		y2 = vert[poly_no[1]].y();
-		z2 = vert[poly_no[1]].z();
-		/* we need a third point to find the normal to the plane */
-		/* so we'll get the end point of the second edge */
-		x3 = vert[poly_no[2]].x();
-		y3 = vert[poly_no[2]].y();
-		z3 = vert[poly_no[2]].z();
-		/* now we calculate the changes between the first and */
-		/* second vertex */
-		dx1 = x2 - x1;
-		dy1 = y2 - y1;
-		dz1 = z2 - z1;
-		/* now changes between third and second vertex */
-		dx2 = x3 - x2;
-		dy2 = y3 - y2;
-		dz2 = z3 - z2;
-		/* at last let's calculate the normal */
-		/* using the cross product */
-		A = (dy1 * dz2) - (dy2 * dz1);
-		B = (dz1 * dx2) - (dz2 * dx1);
-		C = (dx1 * dy2) - (dx2 * dy1);
-		/* now calculate D using a point that is known to be */
-		/* on the plane */
-		D = (A * x1) + (B * y1) + (C * z1);
+		Vector3d v1 = vert[poly_no[1]] - vert[poly_no[0]];
+		Vector3d v2 = vert[poly_no[2]] - vert[poly_no[1]];
 
-		/* now we want to find whether the surface is facing upwards or is */
-		/* facing downwards - this way we apply a different intensity */
-		/* of light source */
-		/* to do this we simply add a value to the y component */
-		normal = (A * x1) + (B * (y1 - 20.0)) + (C * z1) - D;
+		Vector3d normal = v1.cross(v2);
+		normal.normalize();
 
-		if (normal < 0.0)
+		// Calculate a light coming in at an angle for now
+		Vector3d light(0.0f, -5.0f, -1.0f);
+		light.normalize();
+
+		float cos_theta = normal.dot(light);
+
+		if (cos_theta < 0.0)
 		{
-			/* find the angle of incidence between the ray of light */
-			/* (coming straight from below) with the surface */
-			hyp = sqrt((A*A)+(B*B)+(C*C));
-			/* now find the value of cos theta */
-			if ((hyp == 0.0) || (B == 0.0))
-				cos_theta = 0.0;
-			else
-				cos_theta = abs(B) / hyp;
-			/* abs() is used in case the Y(B) value of the normal is negative */
-			/* calculate the diffuse-reflection coeffiecient */
-			ka = (specularity / 100.0) * 0.25f;
-			kd = (specularity / 100.0) * cos_theta;
-			/* now calculate the color intensity - this is facing away from the light source */
-			poly_color[loop] = (color * kd * 0.1f) + Color(ka, ka, ka);
+			// Apply just ambient light to surfaces facing away
+			poly_color[loop] = Color(ka, ka, ka);
 		}
-		else if (normal >= 0.0)
+		else
 		{
-			/* find the angle of incidence between the ray of light */
-			/* (coming straight from above) with the surface */
-			hyp = sqrt((A*A)+(B*B)+(C*C));
-			/* now find the value of cos theta */
-			if ((hyp == 0.0) || (B == 0.0))
-				cos_theta = 0.0;
-			else
-				cos_theta = abs(B) / hyp;
-			/* abs() is used in case the Y(B) value of the normal is negative */
-
-			/* calculate the diffuse-reflection coeffiecient */
+			/* calculate the diffuse-reflection coeffiecient + ambient */
 			ka = (specularity / 100.0) * 0.25f;
 			kd = (specularity / 100.0) * cos_theta;
 			/* now calculate the color intensity */
